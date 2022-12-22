4078d25edc81527001943443bf4b2971d2e370d1
FluffyBullet <davidreynolds34@hotmail.co.uk>
2022-12-22 21:14:58 +0000

    admin options and templates adding

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -17,7 +18,6 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
-
 
     class Meta:
         ordering = ['-created_on']
